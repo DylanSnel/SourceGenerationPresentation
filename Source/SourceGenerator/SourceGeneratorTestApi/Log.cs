46b1e1f85c4cc3f1a060c2ebe769f0bec08c682e
Dylan Snel <dylan@diditsolutions.nl>
2023-10-19 00:23:56 +0200

    Create test project with high performant logging example

---
+++ Source/SourceGenerator/SourceGeneratorTestApi/Log.cs
@@ -0,0 +1,9 @@
+﻿namespace SourceGeneratorTestApi;
+public static partial class Log
+{
+    [LoggerMessage(
+        EventId = 0,
+        Level = LogLevel.Critical,
+        Message = "Weather is shit cause of `{reason}`")]
+    public static partial void WeatherIsBadCauseOf(this ILogger logger, string reason);
+}
